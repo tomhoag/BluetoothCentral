156d89e59baedf532e13f26bf5768b16c8f6ebba
Tom <tomhoag@Gmail.com>
2021-06-17 10:34:43 -0400

    Initial Commit

---
+++ CentralApp/CentralAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralAppApp.swift
+//  CentralApp
+//
+//  Created by Tom on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CentralAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CentralApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CentralApp
+//
+//  Created by Tom on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
